f889ca54dde475b52f7baabf508f64734be4c30c
Andrii Kovalenko <Andrii@invictusgurus.com>
2020-01-31 16:21:41 +0200

    Added dummy tests project.

---
+++ tests/TauCode.WebApi.Host.EasyNetQ.Tests/WebApiHostEasyNetQDummyFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.WebApi.Host.EasyNetQ.Tests
+{
+    [TestFixture]
+    public class WebApiHostEasyNetQDummyFixture
+    {
+        [Test]
+        public void WebApiHostEasyNetQDummyTest()
+        {
+            Assert.Pass("Added for CI.");
+        }
+    }
+}
