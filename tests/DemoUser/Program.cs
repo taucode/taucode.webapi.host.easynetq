c5eb2c8635aef8da5c82eef487a07012c87815ff
Andrey Kovalenko <ak@taucode.com>
2020-01-31 02:36:18 +0200

    Added client; enought for today.

--- tests/DemoUser/Program.cs
+++ tests/DemoUser/Program.cs
@@ -1,12 +1,123 @@
-﻿using System;
+﻿using Autofac;
+using DemoUser.Cli;
+using Serilog;
+using System;
+using TauCode.Cli;
+using TauCode.Cli.Exceptions;
+using TauCode.Mq;
+using TauCode.Mq.EasyNetQ;
 
 namespace DemoUser
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private class ExitException : Exception
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+        private string _name;
+        private IMessagePublisher _publisher;
+        private IMessageSubscriber _subscriber;
+
+        private readonly IContainer _container;
+
+        private static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run();
+        }
+
+        public Program()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterType<EasyNetQMessageSubscriber>().As<IMessageSubscriber>().SingleInstance();
+            containerBuilder.RegisterType<EasyNetQMessagePublisher>().As<IMessagePublisher>().SingleInstance();
+
+            _container = containerBuilder.Build();
+
+        }
+
+        public void Run()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Name: ");
+                    _name = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(_name))
+                    {
+                        throw new Exception("Bad name.");
+                    }
+
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.WriteLine($"Node started with name '{_name}'.");
+
+            _publisher = _container.Resolve<IMessagePublisher>();
+            ((EasyNetQMessagePublisher)_publisher).ConnectionString = "host=localhost";
+
+            _subscriber = _container.Resolve<IMessageSubscriber>();
+            _subscriber.Name = _name;
+            ((EasyNetQMessageSubscriber)_subscriber).ConnectionString = "host=localhost";
+
+
+            _publisher.Start();
+            _subscriber.Start();
+
+            var host = new Host
+            {
+                Output = Console.Out,
+                UserName = _name,
+                Publisher = _publisher,
+            };
+
+            host.AddCustomHandler(
+                () => throw new ExitException(),
+                "exit");
+
+            host.AddCustomHandler(
+                Console.Clear,
+                "cls");
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write(" : ");
+                    var line = Console.ReadLine();
+                    var command = host.ParseLine(line);
+                    host.DispatchCommand(command);
+                }
+                catch (CliCustomHandlerException)
+                {
+                    // ignore
+                }
+                catch (ExitException)
+                {
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            _subscriber.Dispose();
+            _publisher.Dispose();
         }
     }
 }
